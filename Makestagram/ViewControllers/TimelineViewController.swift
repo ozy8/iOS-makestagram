95e5de2e74fe921dc06a4691c5c22f69e97487e4
Ow Zhiyin8 <owzhiyin@gmail.com>
2016-06-30 11:15:31 +0800

    complete chapter 12

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -13,13 +13,44 @@ class TimelineViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var photoTakingHelper: PhotoTakingHelper?
+    var posts: [Post] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tabBarController?.delegate = self
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 1
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        
+        // 2
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        // 3
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        // 4
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        // 5
+        query.includeKey("user")
+        // 6
+        query.orderByDescending("createdAt")
+        
+        // 7
+        query.findObjectsInBackgroundWithBlock {(result: [PFObject]?, error: NSError?) -> Void in
+            // 8
+            self.posts = result as? [Post] ?? []
+            // 9
+            self.tableView.reloadData()
+        }
+    }
     
     func takePhoto() {
         // instantiate photo taking class, provide callback for when photo is selected
@@ -41,6 +72,23 @@ class TimelineViewController: UIViewController {
 }
 
 // MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell")!
+        
+        cell.textLabel!.text = "Post"
+        
+        return cell
+    }
+}
 
 extension TimelineViewController: UITabBarControllerDelegate {
     
